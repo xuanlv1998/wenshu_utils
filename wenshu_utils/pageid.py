59a8937e2e3e70bf7b082d66b875aa4b0c2ad897
阿雄 <489406796@qq.com>
2019-10-28 14:32:56 +0800

    remove

--- wenshu_utils/pageid.py
+++
@@ -1,8 +0,0 @@
-import math
-import random
-
-
-class PageID(str):
-    def __new__(cls):
-        page_id = "".join(hex(math.floor(random.random() * 16))[2:] for _ in range(32))
-        return super(PageID, cls).__new__(cls, page_id)
